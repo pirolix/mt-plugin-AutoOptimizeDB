f83d0c3acb543023ad5502e6660c15158fd8ce27
piroli <piroli@adbff654-c4ea-448a-8ae3-cad037306009>
2009-12-18 21:20:53 +0000

    0.01
    git-svn-id: svn://svn.magicvox.net/var/svn/mt-plugins/AutoOptimizeDB@103 adbff654-c4ea-448a-8ae3-cad037306009

---
+++ AutoOptimizeDB.pl
@@ -0,0 +1,101 @@
+package MT::Plugin::OMV::AutoOptimizeDB;
+
+use strict;
+use MT 4;
+use MT::Object;
+
+use vars qw( $MYNAME $VERSION );
+$MYNAME = 'AutoOptimizeDB';
+$VERSION = '0.01 DEVEL';
+
+use base qw(MT::Plugin);
+my $plugin = __PACKAGE__->new({
+    id => $MYNAME,
+    key => $MYNAME,
+    name => $MYNAME,
+    version => $VERSION,
+    description => <<PERLHEREDOC,
+<__trans phrase="Optimize your database periodically.">
+PERLHEREDOC
+    author_name => 'Open MagicVox.net',
+    author_link => 'http://www.magicvox.net/',
+    doc_link => 'http://www.magicvox.net/',
+});
+MT->add_plugin( $plugin );
+
+sub instance { $plugin; }
+
+### Registry
+sub init_registry {
+    my $plugin = shift;
+    $plugin->registry({
+        tasks => {
+            $MYNAME => {
+                label     => 'Optimize Database',
+                frequency => 60 * 60 * 24 * 30 * 3,
+                code      => \&_hdlr_optimize_database,
+            },
+        },
+    });
+}
+
+### Optimize Database
+sub _hdlr_optimize_database {
+    my ($cb) = @_;
+    return unless lc MT->config('ObjectDriver') eq 'dbi::mysql';
+
+    my @tables = qw(
+        mt_asset
+        mt_asset_meta
+        mt_association
+        mt_author
+        mt_author_meta
+        mt_author_summary
+        mt_blog
+        mt_blog_meta
+        mt_category
+        mt_category_meta
+        mt_comment
+        mt_comment_meta
+        mt_config
+        mt_entry
+        mt_entry_meta
+        mt_entry_rev
+        mt_entry_summary
+        mt_field
+        mt_fileinfo
+        mt_ipbanlist
+        mt_log
+        mt_notification
+        mt_objectasset
+        mt_objectscore
+        mt_objecttag
+        mt_permission
+        mt_placement
+        mt_plugindata
+        mt_role
+        mt_session
+        mt_tag
+        mt_tbping
+        mt_tbping_meta
+        mt_template
+        mt_templatemap
+        mt_template_meta
+        mt_template_rev
+        mt_touch
+        mt_trackback
+        mt_ts_error
+        mt_ts_exitstatus
+        mt_ts_funcmap
+        mt_ts_job
+    );
+
+    my $dbh = MT::Object->driver->rw_handle;
+    foreach (@tables) {
+        my $sql = qq( OPTIMIZE TABLE `$_` );
+        $dbh->do ($sql)
+            or return MT->log ($dbh->errstr || $DBI::errstr);
+    }
+}
+
+1;
